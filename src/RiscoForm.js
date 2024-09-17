21e1c944a067f3ae71b135655eb421b498169422
JOAO-VICTOR-SOARES-LIMA <j.soares@greylogix.com.br>
2024-09-17 17:59:57 -0300

    Estilize os inputs

--- src/RiscoForm.js
+++ src/RiscoForm.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from './axiosConfig';
+import "./RiscoList.css";
 
 const RiscoForm = ({ riscoAtual, onSave }) => {
     const [risco, setRisco] = useState({
@@ -40,30 +41,65 @@ const RiscoForm = ({ riscoAtual, onSave }) => {
     };
 
     return (
+        
         <form onSubmit={handleSubmit} className="form-container">
-            <div className='form-group'>
-            <label htmlFor="tipo">descricao</label>
-            <input type="text" id='descricao' name="descricao" value={risco.descricao} onChange={handleChange} placeholder="Descrição" />
-            </div>
+            
+            <div className='form-input'>
+            <input className='input-descricao' type="text" id='descricao' name="descricao" value={risco.descricao} onChange={handleChange} placeholder="Descrição" />
+            
 
-            <div className='form-group'>
-            <label htmlFor="tipo">Tipo</label>
-            <input type="text" id='tipo' name="tipo" value={risco.tipo} onChange={handleChange} placeholder="Tipo de Risco" />
-            </div>
+                      
+            <input className='input-tipo' type="text" id='tipo' name="tipo" value={risco.tipo} onChange={handleChange} placeholder="Tipo de Risco" />
+        
+
             
+            <input className='input-probabilidade' type="text" id='probabilidade' name="probabilidade" value={risco.probabilidade} onChange={handleChange} placeholder="Probabilidade" />
             
-            <input type="text" id='probabilidade' name="probabilidade" value={risco.probabilidade} onChange={handleChange} placeholder="Probabilidade" />
-            <input type="text" id='area' name="area" value={risco.area} onChange={handleChange} placeholder="Área" />
-            <input type="text" id='classificacao' name="classificacao" value={risco.classificacao} onChange={handleChange} placeholder="Classificação" />
-            <input type="text" id='projeto' name="projeto" value={risco.projeto} onChange={handleChange} placeholder="Projeto" />
-            <input type="date" id='data_entrada' name="data_entrada" value={risco.data_entrada} onChange={handleChange} placeholder="Data de Entrada" />
-            <input type="text" id='impacto' name="impacto" value={risco.impacto} onChange={handleChange} placeholder="Impacto" />
-            <input type="text" id='consequencia' name="consequencia" value={risco.consequencia} onChange={handleChange} placeholder="Consequência" />
-            <input type="text" id='jalon_afetado' name="jalon_afetado" value={risco.jalon_afetado} onChange={handleChange} placeholder="Jalon Afetado" />
-            <input type="text" id='metier' name="metier" value={risco.metier} onChange={handleChange} placeholder="Metier" />
-            <input type="text" id='status' name="status" value={risco.status} onChange={handleChange} placeholder="Status" />
+
+            
+            <input className='input-area' type="text" id='area' name="area" value={risco.area} onChange={handleChange} placeholder="Área" />
+            
+
+            
+            <input className='input-classificacao' type="text" id='classificacao' name="classificacao" value={risco.classificacao} onChange={handleChange} placeholder="Classificação" />
+            
+
+            
+            <input className='input-projeto' type="text" id='projeto' name="projeto" value={risco.projeto} onChange={handleChange} placeholder="Projeto" />
+            
+
+            
+            <input className='input-data' type="date" id='data_entrada' name="data_entrada" value={risco.data_entrada} onChange={handleChange} placeholder="Data de Entrada" />
+            
+
+            
+            <input className='input-impacto' type="text" id='impacto' name="impacto" value={risco.impacto} onChange={handleChange} placeholder="Impacto" />
+            
+
+            
+            <input className='input-consequencia' type="text" id='consequencia' name="consequencia" value={risco.consequencia} onChange={handleChange} placeholder="Consequência" />
+            
+
+            
+            <input className='input-jalon' type="text" id='jalon_afetado' name="jalon_afetado" value={risco.jalon_afetado} onChange={handleChange} placeholder="Jalon Afetado" />
+            
+
+            
+            <input className='input-metier' type="text" id='metier' name="metier" value={risco.metier} onChange={handleChange} placeholder="Metier" />
+            
+
+           
+            <input className='input-status' type="text" id='status' name="status" value={risco.status} onChange={handleChange} placeholder="Status" />
+            
+
+            
             <input type="text" id='id_usuario' name="id_usuario" value={risco.id_usuario} onChange={handleChange} placeholder="Usuário" />
             <button type="submit" id='descricao' className='button-salvar'>Salvar</button>
+            </div>
+            
+ 
+            
+            
         </form>
     );
 };
